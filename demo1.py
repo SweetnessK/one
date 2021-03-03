925900caab1ed66b1d02e37ba4a482c7ff8eacb4
qianqian <1271169640@qq.com>
2021-03-03 15:28:03 +0800

    第一次向GitHub传文件

---
+++ demo1.py
@@ -0,0 +1,27 @@
+
+"""
+print("hello word")#字符串
+print(())#元祖
+print([])#数组
+print({})#字典
+print("hahha","hahahahha","12324")
+print("哈哈"+"xixi")
+print("不是吧"*100)
+name="张思"
+print(name)
+"""
+# print(type(222))
+# print(type(2.324))
+# print(type([]))
+# print(type(True))
+
+# a = float(input("请输入a的值"))
+# b = float(input("请输入b的值"))
+# print("a+b= ",a+b)
+
+
+a = int(len(input("请输入a的值")))
+b = int(len(input("请输入b的值")))
+print("a+b字符长度=",a+b)
+
+
